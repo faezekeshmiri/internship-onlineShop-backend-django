72c72f3152f5c7af2c9f1e07e10f46d93ff0e44c
faeze <keshmirifaezeh@gmail.com>
2020-10-04 18:11:45 +0330

    Create users app and products app views. Closes #5

--- online_shop/users/views.py
+++ online_shop/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ShoppingCart, Address
+from .serializers import ShoppingCartSerializer, AddressSerializer, CustomUserSerializer
+from django.conf import settings
 
-# Create your views here.
+
+class ShoppingCartView(viewsets.ModelViewSet):
+    queryset = ShoppingCart.objects.all()
+    serializer_class = ShoppingCartSerializer
+
+
+class CustomUserView(viewsets.ModelViewSet):
+    queryset = settings.AUTH_USER_MODEL.objects.all().order_by('date_joined')
+    serializer_class = CustomUserSerializer
+
+
+class AddressView(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
